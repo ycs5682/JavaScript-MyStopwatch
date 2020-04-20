3617aa80c7b053e79f0fa47a3db562c2987369c7
ycs5682 <c-yu@creationline.com>
2020-04-20 17:31:31 +0900

    タイマーを表示する

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'user strict';
+'use strict';
 
 {
   const timer = document.getElementById('timer');
@@ -9,11 +9,17 @@
   let startTime;
 
   function countUp() {
-    console.log(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s= String(d.getSeconds()).padStart(2,'0');
+    const ms= String(d.getMilliseconds()).padStart(3,'0');
+    timer.textContent = `${m}:${s}.${ms}`;
+
+
 
     setTimeout(() => {
       countUp();
-    }, 1000);
+    }, 10);
   }
   start.addEventListener('click', () => {
     startTime = Date.now();
