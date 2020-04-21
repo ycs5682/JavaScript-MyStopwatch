6691693918e1edec0bd2d38ccc74514f2ad29dfd
ycs5682 <c-yu@creationline.com>
2020-04-21 09:37:57 +0900

    ボタンの有無/無効を切り替える

--- js/main.js
+++ js/main.js
@@ -23,17 +23,42 @@
       countUp();
     }, 10);
   }
+
+  function setButtonStateInitial() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+    
+  }
+
+  function setButtonStateRunning() {
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+
+  function setButtonStateStopped() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
+
+  setButtonStateInitial()
+
   start.addEventListener('click', () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
   
   stop.addEventListener('click', () => {
+    setButtonStateStopped();
     clearTimeout(timeoutId);
     elapsedTime = Date.now() - startTime;
   });
   
   reset.addEventListener('click', () => {
+    setButtonStateStopped();
     timer.textContent = '00:00.000';
     elapsedTime = 0;
   });
